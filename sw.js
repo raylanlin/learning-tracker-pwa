a09603c61de4cba7587e72c4339a8881b60be20a
Raylan LIN <linrion101@gmail.com>
2026-04-18 21:26:04 +0800

    chore: bump sw cache version to v3 (force update)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'learning-tracker-v2';
+const CACHE_NAME = 'learning-tracker-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
