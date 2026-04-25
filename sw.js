ad7426a291a673ebd6d06116033e45446c0a3018
Raylan LIN <linrion101@gmail.com>
2026-04-25 09:07:50 +0800

    bump cache version v3→v4, index.html hash change triggers SW update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'learning-tracker-v3';
+const CACHE_NAME = 'learning-tracker-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
